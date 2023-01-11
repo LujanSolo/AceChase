e8415071dc129718af2828c8293462f02414055f
LujanSolo <lujansolo@proton.me>
2023-01-11 12:10:07 -0800

    pseudocoding for createCard function

--- index.js
+++ index.js
@@ -18,8 +18,27 @@ function createCard(cardItem){
   const cardFrontImg = createElement('img')
   const cardBackImg = createElement('img')
 
+  //add class and id to card element
+  addClassToElement(cardElem, 'card')
+  addIdToElement(cardElem, cardItem.id)
+
+  //add class to inner card element
+  addClassToElement(cardInnerElem, 'card-inner')
+
+  //add class to front card element
+  
+  //add class to back card element
+
+  //add src attribute and appropriate value to img element - back of card
 }
 
 function createElement(elemType){
   return document.createElement(elemType)
+}
+
+function addClassToElement(elem, className){
+  elem.classList.add(className)
+}
+function addIdToElement(elem, id){
+
 }
